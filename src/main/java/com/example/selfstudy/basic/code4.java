6796fb5d45a21aba8c4b26cebed56a09da6d7608
taewoocode <qkrxodn6035@gmail.com>
2023-11-01 17:43:37 +0900

    gg

--- src/main/java/com/example/selfstudy/basic/code4.java
+++ src/main/java/com/example/selfstudy/basic/code4.java
@@ -1,2 +1,14 @@
-package com.example.selfstudy.basic;public class code4 {
-}
+package com.example.selfstudy.basic;
+import java.util.Scanner;
+public class code4 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String a = sc.next();
+            int l = a.length();
+            for(int i = 0; i<l; i++){
+                System.out.println(a.charAt(i)); //문자열 a의 i번째를 반환해준다.
+            }
+
+        }
+    }
+
--- src/main/java/com/example/selfstudy/basic/code5.java
+++ src/main/java/com/example/selfstudy/basic/code5.java
@@ -1,2 +1,15 @@
-package com.example.selfstudy.basic;public class code5 {
-}
+package com.example.selfstudy.basic;
+import java.util.Scanner;
+
+public class code5 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            if(n%2 == 0){
+                System.out.print(n+" is even");
+            }else{
+                System.out.print(n+" is odd");
+            }
+        }
+    }
+
