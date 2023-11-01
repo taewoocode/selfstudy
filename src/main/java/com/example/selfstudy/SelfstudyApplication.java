c6513b3b120dd2e2c017788247c0fcb0f5b5195c
taewoocode <qkrxodn6035@gmail.com>
2023-11-01 13:30:11 +0900

    hi

---
+++ src/main/java/com/example/selfstudy/SelfstudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.selfstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfstudyApplication.class, args);
+	}
+
+}
